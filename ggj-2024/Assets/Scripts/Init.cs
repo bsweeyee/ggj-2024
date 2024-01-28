9f6ab24d04f6119b3144f6af3874d6dc3c5cf845
Brandon Swee <sweeyee3@gmail.com>
2024-01-28 15:38:03 +0100

    remove init

--- ggj-2024/Assets/Scripts/Init.cs
+++ ggj-2024/Assets/Scripts/Init.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Init : MonoBehaviour
 {
-    [RuntimeInitializeOnLoadMethod]
-    public static void InitGame() {
-        if (FindObjectOfType<Game>() == null) {            
-            var game = Resources.Load<Game>("GameManager");
-            Instantiate(game);                    
-        }
-    }
+    // [RuntimeInitializeOnLoadMethod]
+    // public static void InitGame() {
+    //     if (FindObjectOfType<Game>() == null) {            
+    //         var game = Resources.Load<Game>("GameManager");
+    //         Instantiate(game);                    
+    //     }
+    // }
 }
